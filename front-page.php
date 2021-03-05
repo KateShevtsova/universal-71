18c1d1a5d6a2b98f8b3cccaba8dbb8412e267c0b
KateShevtsova <serpintera@mail.ru>
2021-03-06 00:24:48 +0300

    lesson-4

---
+++ front-page.php
@@ -0,0 +1,60 @@
+<?php get_header()?>
+<main class="front-page-header">
+  <div class="container">
+    <div class="hero">
+      <div class="left">
+       <img src="<?php echo get_template_directory_uri() . '/assets/images/image.jpeg' ?>" alt="" class="post-thumb">
+       <a href="#" class="author">
+         <img src="<?php echo get_template_directory_uri() . '/assets/images/avatar.png'?>" alt="" class="avatar">
+         <div class="author-bio">
+           <span class="author-name">Имя автора</span>
+           <span class="author-rank">Должность</span>
+         <!-- class="author-bio" -->
+         </div>
+       </a>
+       <div class="post-text">
+          <a href="#" class="category-name">Рубрики</a>
+          <h2 class="post-title">Название поста</h2>
+          <a href="#" class="more">Читать далее</a>
+
+       <!-- class="post-text" -->
+       </div>
+      <!-- class="left" -->
+      </div>
+
+      <div class="right">
+       <h3 class="recommend">Рекомендуем</h3>
+       <ul class="posts-list">
+        <li class="post">
+          <span class="category-name">Категория</span>
+          <h4 class="post-title">Название поста в две строки</h4>
+        </li>
+        <li class="post">
+          <span class="category-name">Категория</span>
+          <h4 class="post-title">Название поста в две строки</h4>
+        </li>
+        <li class="post">
+          <span class="category-name">Категория</span>
+          <h4 class="post-title">Название поста в две строки</h4>
+        </li>
+        <li class="post">
+          <span class="category-name">Категория</span>
+          <h4 class="post-title">Название поста в две строки</h4>
+        </li>
+        <li class="post">
+          <span class="category-name">Категория</span>
+          <h4 class="post-title">Название поста в две строки</h4>
+        </li>
+       </ul>
+
+
+
+      <!-- class="right" -->
+      </div>
+
+     <!-- class="hero" -->
+    </div>
+  <!-- class="container" -->
+ </div>
+</main>
+
