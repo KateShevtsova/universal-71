c06237d92aca60ebf84b8157fcd50f4627a33ef7
KateShevtsova <serpintera@mail.ru>
2021-03-09 16:45:27 +0300

    lesson-6

--- front-page.php
+++ front-page.php
@@ -236,7 +236,7 @@
                           <a href="<?php the_permalink()?>" class="article-grid-permalink">
                             <img src="<?php echo get_the_post_thumbnail_url()?>" alt="" class="article-grid-thumb" >
                             <h4 class="article-grid-title">
-                              <?php echo mb_strimwidth(get_the_title(), 0, 50, '...')?>
+                              <?php echo get_the_title()?>
                           </a>
                       </li>
                       <?php 
@@ -247,7 +247,7 @@
                       <li class="article-grid-item article-grid-item-default">
                         <a href="<?php the_permalink()?>" class="article-grid-permalink">
                           <h4 class="article-grid-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...')?></h4>
-                          <p class="article-grid-excerpt"><?php echo get_the_excerpt()?></p>
+                          <p class="article-grid-excerpt"><?php echo mb_strimwidth(get_the_excerpt(), 0, 75, '...')?></p>
                           <span class="article-date"><?php the_time('j F')?></span>
                         </a>
                       </li>
